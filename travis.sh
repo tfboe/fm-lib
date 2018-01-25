6641f7c0c377395a44358f82fe3f92e430fa82db
blacktemplar <blacktemplar@a1.net>
2018-01-25 17:49:44 +0100

    improves travis scripts

--- travis.sh
+++ travis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ "$CODECOVERAGE" = '1' ]; then
+CODE_COVERAGE="${CODE_COVERAGE:-0}"
+
+if [ "$CODE_COVERAGE" = '1' ]; then
     vendor/bin/phpunit --stderr --configuration phpunit-unit.xml --coverage-clover=coverage.xml
     if [ $? -eq 0 ]; then bash <(curl -s https://codecov.io/bash); fi
 else
