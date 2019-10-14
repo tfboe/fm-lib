e3addf4a32d05b65c4d1984d21db5d0bf4657afd
blacktemplar <blacktemplar@a1.net>
2019-10-14 11:35:03 +0200

    add SpecificationHandler

---
+++ src/Helpers/SpecificationHandler.php
@@ -0,0 +1,160 @@
+<?php
+
+
+namespace Tfboe\FmLib\Helpers;
+
+
+use Closure;
+use DateTime;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Tfboe\FmLib\Entity\Helpers\BaseEntityInterface;
+use Tfboe\FmLib\Http\Controllers\BaseController;
+
+trait SpecificationHandler
+{
+//<editor-fold desc="Fields">
+  /**
+   * @var string
+   */
+  private $datetimetzFormat = 'Y-m-d H:i:s e';
+//</editor-fold desc="Fields">
+
+//<editor-fold desc="Protected Final Methods">
+  /**
+   * @return string
+   */
+  protected final function getDatetimetzFormat(): string
+  {
+    return $this->datetimetzFormat;
+  }
+//</editor-fold desc="Protected Final Methods">
+
+//<editor-fold desc="Protected Methods">
+  /**
+   * Gets a transformation function which transforms a string in datetime format into a datetime with the given timezone
+   * @return Closure the function which transforms a string into a datetime
+   */
+  protected function datetimetzTransformer(): Closure
+  {
+    return TransformerFactory::datetimetzTransformer($this->datetimetzFormat);
+  }
+
+  /**
+   * Gets a transformation function which transforms an enum name into the corresponding value
+   * @param string $enumName the name of the enum
+   * @return Closure the function which transforms a name into the enum value
+   */
+  protected function enumTransformer(string $enumName): Closure
+  {
+    return TransformerFactory::enumTransformer($enumName);
+  }
+
+  /**
+   * @param $class
+   * @param $id
+   * @return mixed
+   */
+  abstract protected function getReference($class, $id);
+
+  /**
+   * Fills an object with the information of inputArray
+   * @param BaseEntityInterface $object the object to fill
+   * @param array $specification the specification how to fill the object
+   * @param array $inputArray the input array
+   * @return mixed the object
+   */
+  protected function setFromSpecification(BaseEntityInterface $object, array $specification, array $inputArray)
+  {
+    foreach ($specification as $key => $values) {
+      if (!array_key_exists('ignore', $values) || $values['ignore'] != true) {
+        $matches = [];
+        preg_match('/[^\.]*$/', $key, $matches);
+        $arrKey = $matches[0];
+        if (array_key_exists('property', $values)) {
+          $property = $values['property'];
+        } else {
+          $property = $arrKey;
+        }
+        $setter = 'set' . ucfirst($property);
+        if (array_key_exists($arrKey, $inputArray)) {
+          $value = $inputArray[$arrKey];
+          $this->transformValue($value, $values);
+          $object->$setter($value);
+        } else if (array_key_exists('default', $values) && $object->methodExists($setter)) {
+          $object->$setter($values['default']);
+        }
+      }
+    }
+    return $object;
+  }
+
+  /**
+   * Transforms the given value based on different configurations in specification.
+   * @param mixed $value the value to optionally transform
+   * @param array $specification the specification for this value
+   */
+  protected function transformValue(&$value, array $specification)
+  {
+    if (array_key_exists('nullValue', $specification) && $value === null) {
+      $value = $specification['nullValue'];
+    }
+    if (array_key_exists('reference', $specification)) {
+      $value = $this->getReference($specification['reference'], $value);
+    }
+    if (array_key_exists('type', $specification)) {
+      $value = self::transformByType($value, $specification['type']);
+    }
+    if (array_key_exists('transformer', $specification)) {
+      $value = $specification['transformer']($value);
+    }
+  }
+
+  /**
+   * Validates the parameters of a request by the validate fields of the given specification
+   * @param Request $request the request
+   * @param array $specification the specification
+   * @return $this|BaseController
+   * @throws ValidationException
+   */
+  protected function validateBySpecification(Request $request, array $specification): BaseController
+  {
+    $arr = [];
+    foreach ($specification as $key => $values) {
+      if (array_key_exists('validation', $values)) {
+        $arr[$key] = $values['validation'];
+      }
+      if (array_key_exists('transformBeforeValidation', $values) && $request->has($key)) {
+        $request->merge([$key => $values['transformBeforeValidation']($request->get($key))]);
+      }
+    }
+    $this->validate($request, $arr);
+    return $this;
+  }
+
+  /**
+   * @param Request $request
+   * @param array $spec
+   * @return mixed
+   */
+  abstract protected function validateSpec(Request $request, array $spec);
+//</editor-fold desc="Protected Methods">
+//</editor-fold desc="Protected Methods">
+
+//<editor-fold desc="Private Methods">
+  /**
+   * Transforms a value from a standard json communication format to its original php format. Counter part of
+   * valueToJson().
+   * @param string $value the json representation of the value
+   * @param string $type the type of the value
+   * @return mixed the real php representation of the value
+   */
+  private static function transformByType($value, $type)
+  {
+    if (strtolower($type) === 'date' || strtolower($type) === 'datetime') {
+      return new DateTime($value);
+    }
+    return $value;
+  }
+//</editor-fold desc="Private Methods">
+}
